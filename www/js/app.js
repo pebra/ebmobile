b9020f6e9b5e10860a145164f3749791131e3075
Peter Brändel <peter@bitbites.de>
2013-11-02 17:34:42 +0100

    Favorites not quite working

--- www/js/app.js
+++ www/js/app.js
@@ -19,9 +19,15 @@ App.Job = DS.Model.extend({
   firm: DS.attr(),
   location: DS.attr(),
   title: DS.attr(),
-  link: DS.attr(),
-  mobile_logo_url: DS.attr()
+  job_link: DS.attr(),
+  mobile_logo_url: DS.attr(),
+  favorite: DS.attr()
 });
+
+App.Favorite = DS.Model.extend({
+  job_id: DS.attr()
+});
+
 
 App.Router.map(function() {
   this.resource('jobs', function() {
@@ -29,6 +35,7 @@ App.Router.map(function() {
   this.resource('search', function() {
     this.route('jobs');
   });
+  this.route('favorites');
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -37,21 +44,55 @@ App.IndexRoute = Ember.Route.extend({
   }
 });
 
+App.FavoritesRoute = Ember.Route.extend({
+  model: function() {
+    App.store = this.store;
+    var self = this;
+    App.favs = Ember.A();
+
+    App.favids = this.store.findAll('favorite');//.map(function(fav) { return fav.get('job_id')});
+    var url = "http://www.empfehlungsbund.de/api/joblist.jsonp?callback=json_callback&ids="
+    Ember.$.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: function(jobs) {
+        console.log(jobs);
+        jobs.map(function(job) {
+          job.job_id = job.id;
+          job.mobile_log_url = "http://" + job.mobile_logo_url;
+          console.log("JAB" + job.id);
+          App.favs.pushObject(App.store.createRecord('job', job));
+          console.log(url);
+        });
+        return jobs; 
+      }
+    });
+    return App.favs;
+  }
+});
+
 App.JobsJobRoute = Ember.Route.extend({
+  onList: false,
+  notification: '',
   model: function(params) {
     var store = this.store;
-    //http://www.empfehlungsbund.de/api/job.jsonp?callback=json_callback&id
     var id = params.job_id;
+    var foundJob = '';
     return Ember.$.ajax({
       url: "http://www.empfehlungsbund.de/api/job.jsonp?callback=json_callback&id=" +id,
       dataType: 'jsonp',
       success: function(job) {
-        console.log(job);
-        job.job_id = job.id;
-        job.mobile_log_url = "http://" + job.mobile_logo_url;
-        return job
+        foundJob = store.find('job', { job_id: job.id});
       }
     });
+  },
+
+  actions: {
+    addToFavorites: function(job_id) {
+      this.store.find('job', {job_id: job_id}).set('favorite', true);
+      this.get('controller').set('notification', 'Diese Stelle befindet sich nun auf Ihrer Merkliste.');
+      this.get('controller').set('onList', true);
+    }
   }
 });
 
@@ -70,7 +111,9 @@ App.SearchRoute = Ember.Route.extend({
         success: function(jobsResults) {
           var san = jobsResults.map(function(set) { set.job_id = set.id; set.mobile_logo_url = "http://" + set.mobile_logo_url; return set; });
           san.forEach(function(data) {
-            jobs.pushObject(self.store.createRecord('job', data));
+            var job = self.store.createRecord('job', data);
+            job.save();
+            jobs.pushObject(job);
           });
           self.get('controller').set('jobsResults', jobs);
           return jobs;
