4884495a1ecf9d35167bcdabb49e4748260aa3cd
Peter Brändel <peter@bitbites.de>
2014-01-09 21:53:49 +0100

    Further work on fries

--- ebapp/source/js/_app.coffee
+++ ebapp/source/js/_app.coffee
@@ -116,3 +116,9 @@ App.SearchJobsRoute = Ember.Route.extend(
   model: ->
     @store.findAll "job"
 )
+
+Ember.Handlebars.helper 'truncate', (str, len) ->
+  if (str.length > len)
+    return str.substring(0, len - 3) + '...'
+  else
+    return str
--- ebapp/source/js/all.js
+++ ebapp/source/js/all.js
@@ -1,9 +1,8 @@
 //= require _index
 //= require jquery-1.9.1.js
-//= require bootstrap.min.js
+//= require fries.js
 //= require handlebars-1.0.0.js
 //= require ember-1.1.2.js
 //= require ember-data.js
-//= require fries.js
 //= require localstorage_adapter.js
 //= require _app
