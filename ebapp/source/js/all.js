5d3bac0326ac1a41b5619a4b5b622b7c63724e7e
Stefan Wienert <stefan.wienert@pludoni.de>
2014-03-12 20:12:47 +0100

    Eigene Files geloescht -> durch bower components ersetzt

--- ebapp/source/js/all.js
+++ ebapp/source/js/all.js
@@ -1,8 +1,4 @@
 //= require _index
-//= require jquery-1.9.1.js
-//= require fries.js
-//= require handlebars-1.0.0.js
-//= require ember-1.1.2.js
-//= require ember-data.js
-//= require localstorage_adapter.js
+//= require /bower_components/jquery/dist/jquery.min.js
+//= require /bower_components/fries/dist/fries.js
 //= require _app
