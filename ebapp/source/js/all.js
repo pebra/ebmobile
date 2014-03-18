a03a9f96a0f6f582a75c77ff379919032c7b9fa1
Stefan Wienert <stefan.wienert@pludoni.de>
2014-03-18 16:41:27 +0100

    finalized

--- ebapp/source/js/all.js
+++ ebapp/source/js/all.js
@@ -8,6 +8,5 @@
 //= require angularjs-geolocation/dist/angularjs-geolocation.min.js
 //= require angularLocalStorage/src/angularLocalStorage
 //= require leaflet/dist/leaflet-src.js
-//= require ubilabs-geocomplete/jquery.geocomplete.js
 //= require _app
 //= require_tree ./app/
