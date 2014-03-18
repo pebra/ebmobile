6bb674fb5bad385d8a0f7b402e2c3b0c56add4be
Stefan Wienert <stefan.wienert@pludoni.de>
2014-03-18 15:13:55 +0100

    leaflet wieder rein

--- ebapp/source/js/all.js
+++ ebapp/source/js/all.js
@@ -7,7 +7,7 @@
 //= require angular-cookies/angular-cookies
 //= require angularjs-geolocation/dist/angularjs-geolocation.min.js
 //= require angularLocalStorage/src/angularLocalStorage
-// require leaflet/dist/leaflet-src.js
+//= require leaflet/dist/leaflet-src.js
 //= require ubilabs-geocomplete/jquery.geocomplete.js
 //= require _app
 //= require_tree ./app/
